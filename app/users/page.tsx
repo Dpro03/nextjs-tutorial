5700dbdf4c5fbbeecbd3bc121e25a24346c8a65f
David Proto <davidproto@sbcglobal.net>
2024-02-09 18:25:35 -0500

    first commit

---
+++ app/users/page.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+interface User {
+  id: number;
+  name: string;
+  address: { street: string; city: string };
+  email: string; 
+}
+
+const UsersPage = async () => {
+  const res = await fetch(
+    'https://jsonplaceholder.typicode.com/users', { cache: 'no-store' }
+  );
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <table className='table table-bordered'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td> 
+              <td>{user.address.street}, {user.address.city}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default UsersPage;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,9 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['light', 'cyberpunk', 'dark', 'night', 'winter', 'luxury'],
+  }
 }
 export default config
